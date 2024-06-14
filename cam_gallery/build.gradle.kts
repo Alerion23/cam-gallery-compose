2ee674c0606aeb204cfe94542429b00132c8812f
Alerion23 <lars2142@gmail.com>
2024-06-14 19:59:54 +0300

    Fixed

--- cam_gallery/build.gradle.kts
+++ cam_gallery/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -61,7 +61,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.Alerion23"
             artifactId = "cam-gallery"
-            version = "1.0.4"
+            version = "1.0.5"
             afterEvaluate {
                 from(components["release"])
             }
